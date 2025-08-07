ffb7a1a822851b717e535d65ffeba28b575dcc0c
FEZPEZ <131315603+FEZPEZ@users.noreply.github.com>
2025-08-06 23:48:32 -0700

    Restart website for Google indexing

--- guestHandler.js
+++
@@ -1,9 +0,0 @@
-const addGuestBtn = document.getElementById('addGuestBtn');
-const guestsDiv = document.getElementById('guests');
-
-addGuestBtn.addEventListener('click', () => {
-    const guestInput = document.createElement('div');
-    guestInput.classList.add('guest-input');
-    guestInput.innerHTML = '<input type="text" name="guest[]" placeholder="Guest Name">';
-    guestsDiv.appendChild(guestInput);
-});
